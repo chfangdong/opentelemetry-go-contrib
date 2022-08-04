c80d3581019ae7c186e7759cea89fb53d24b7458
69644 <fangdong.cheng@bangdao-tech.com>
2022-08-04 09:54:16 +0800

    restore otelhttp

--- instrumentation/net/http/otelhttp/handler.go
+++ instrumentation/net/http/otelhttp/handler.go
@@ -207,6 +207,12 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	// Add metrics
 	attributes := append(labeler.Get(), semconv.HTTPServerMetricAttributesFromHTTPRequest(h.operation, r)...)
+
+	//self add status_code
+	if rww.statusCode > 0 {
+		attributes = append(attributes, semconv.HTTPAttributesFromHTTPStatusCode(rww.statusCode)...)
+	}
+
 	h.counters[RequestContentLength].Add(ctx, bw.read, attributes...)
 	h.counters[ResponseContentLength].Add(ctx, rww.written, attributes...)
 
